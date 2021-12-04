32fefeaa0e5f34307e495ea90b80b396c98af9d4
Teky6606 <95511675+Teky6606@users.noreply.github.com>
2021-12-04 08:46:59 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.showNumber(input.acceleration(Dimension.X))
+    basic.pause(2000)
+})
